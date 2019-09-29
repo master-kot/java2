4c535dbedfc8fc482d3b9ba7c1fa83291a7ba9da
master-kot <master-kot@mail.ru>
2019-09-29 18:05:18 +0300

    Creation of PhoneBook was completed, but it can contain only one number for each person. It needs to add the ArrayList for saving more numbers

--- lesson3/Main.java
+++ lesson3/Main.java
@@ -1,18 +1,42 @@
 package lesson3;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+/*
 class PhoneBook {
-    private HashMap<String, Person> phonebook = new HashMap<>();
+    private HashMap<String, lesson3.simple.Person> phonebook = new HashMap<>();
+    private class dataPerson {
 
-    void setPhoneBook (Person[] person) {
+    }
+*/
+/*    void setPhoneBook (Person[] person) {
         for (Person value : person) {
             phonebook.put(value.getName(), value);
         }
+    }*/
+/*
+    void setPhoneBook (lesson3.simple.Person[] person) {
+        for (lesson3.simple.Person value : person) {
+            if (phonebook.containsKey(value.getName()) ) {
+                int j = mapArrayParts.get(s);
+                mapArrayParts.replace(s, ++j);
+            } else {
+                phonebook.put(value.getName(), value);
+            }
+
+        }
     }
 
-    void setPhoneBook (Person person) {
-            phonebook.put(person.getName(), person);
+
+
+    void setPhoneBook (lesson3.simple.Person person) {
+        if ( phonebook.containsKey(person.getName()) ) {
+
+            phonebook.replace(person.getName(), changedPerson;)
+        } else {
+            phonebook.put(person.getName(), personData);
+        }
     }
 
     void getPhone (String name) {
@@ -34,17 +58,21 @@ class PhoneBook {
 
 class Person {
     private String name;
-    private int phone;
+    private ArrayList<Integer> phone = new ArrayList<>();;
     private String email;
 
     Person (String name, int phone, String email){
         this.name = name;
-        this.phone = phone;
+        this.phone.add(phone);
         this.email = email;
     }
 
-    int getPhone() {
+    ArrayList<Integer> getPhone() {
         return phone;
+    }
+
+    void addPhone(int phone) {
+        this.phone.add(phone);
     }
 
     String getEmail() {
@@ -54,24 +82,24 @@ class Person {
     String getName() {
         return name;
     }
-}
+}*/
 
 public class Main {
     public static void main(String[] args) {
-        Person[] person = new Person[5];
-        person[0] = new Person("Ivanov", 111111, "111111@ya.ru");
-        person[1] = new Person("Petrov", 222222, "222222@ya.ru");
-        person[2] = new Person("Sidorov", 333333, "333333@ya.ru");
-        person[3] = new Person("Kozlov", 444444, "444444@ya.ru");
-        person[4] = new Person("Zaytsev", 555555, "555555@ya.ru");
-        Person p5 = new Person("Ivanov", 666666, "666666@ya.ru");
+/*        lesson3.simple.Person[] person = new lesson3.simple.Person[5];
+        person[0] = new lesson3.simple.Person("Ivanov", 111111, "111111@ya.ru");
+        person[1] = new lesson3.simple.Person("Petrov", 222222, "222222@ya.ru");
+        person[2] = new lesson3.simple.Person("Sidorov", 333333, "333333@ya.ru");
+        person[3] = new lesson3.simple.Person("Kozlov", 444444, "444444@ya.ru");
+        person[4] = new lesson3.simple.Person("Zaytsev", 555555, "555555@ya.ru");
+        lesson3.simple.Person p5 = new lesson3.simple.Person("Ivanov", 666666, "666666@ya.ru");
 
-        PhoneBook myBook = new PhoneBook();
+        lesson3.simple.PhoneBook myBook = new lesson3.simple.PhoneBook();
         myBook.setPhoneBook(person);
         myBook.setPhoneBook(p5);
         myBook.getPhone("Ivanov");
         myBook.getEmail("Ivanov");
         myBook.getPhone("Bobrov");
         myBook.getEmail("Bobrov");
-    }
+*/    }
 }
