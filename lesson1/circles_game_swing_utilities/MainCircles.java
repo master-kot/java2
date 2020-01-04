bdaf2ef9bef7c5327912657552483e4d867dea84
master-kot <master-kot@mail.ru>
2020-01-04 13:38:17 +0300

    homework for lessons 1 modified

---
+++ lesson1/circles_game_swing_utilities/MainCircles.java
@@ -0,0 +1,86 @@
+package lesson1.circles_game_swing_utilities;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
+public class MainCircles extends JFrame {
+    /*
+    * Полностью разобраться с кодом, написать класс Бэкграунд, изменяющий цвет канвы в зависимости от времени
+	* Реализовать добавление новых кружков по клику используя ТОЛЬКО массивы
+	* Реализовать по клику другой кнопки удаление кружков (никаких эррейЛист)
+    */
+
+    private static final int POS_X = 600;
+    private static final int POS_Y = 200;
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    private Sprite[] sprites = new Sprite[1];
+    private int spritesCount = 0;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainCircles();
+            }
+        });
+    }
+
+    MainCircles() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("Circles");
+
+        GameCanvas gameCanvas = new GameCanvas(this);
+        gameCanvas.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON1)
+                    addSprite(new Ball(e.getX(), e.getY()));
+                else if (e.getButton() == MouseEvent.BUTTON3)
+                    removeSprite();
+            }
+        });
+        initApplication();
+        add(gameCanvas, BorderLayout.CENTER);
+        setVisible(true);
+    }
+
+    private  void removeSprite() {
+        if (spritesCount > 1)
+            spritesCount--;
+    }
+
+    private void addSprite(Sprite s) {
+        if (spritesCount == sprites.length) {
+            Sprite[] newSprites = new Sprite[sprites.length * 2];
+            System.arraycopy(sprites, 0, newSprites, 0, sprites.length);
+            sprites = newSprites;
+        }
+        sprites[spritesCount++] = s;
+    }
+
+    private void initApplication() {
+        addSprite(new Background());
+    }
+
+    void onDrawFrame(GameCanvas canvas, Graphics g, float deltaTime) {
+        update(canvas, deltaTime);
+        render(canvas, g);
+    }
+
+    private void update(GameCanvas canvas, float deltaTime){
+        for (int i = 0; i < spritesCount; i++) {
+            sprites[i].update(canvas, deltaTime);
+        }
+    }
+
+    private void render(GameCanvas canvas, Graphics g) {
+        for (int i = 0; i < spritesCount; i++) {
+            sprites[i].render(canvas, g);
+        }
+    }
+}
